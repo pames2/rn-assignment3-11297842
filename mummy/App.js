ba1d72c15d035cb5122874b6d20f2d0a86985245
death-inspires-me <nana.esseney@gmail.com>
2024-05-31 21:39:01 +0000

    added searchbar

--- mummy/App.js
+++ mummy/App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import { Feather } from "@expo/vector-icons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.topSection}>
+        <View style={styles.Hello}>
+      <Text style={styles.helloText}>Hello, Devs</Text>
+      <Text style={styles.smallerText}>14 Tasks Today</Text>
+      </View>
+      <Image style={styles.avatar} source={require('./profileImage.png')} /> 
+      </View>
+
+      <View style={styles.search}>
+      <View style={styles.searchBar}>
+        <Feather
+          name="search"
+          size={20}
+          color="black"
+          style={{ marginLeft: 1 }}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Search"
+        />
+      </View>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +36,58 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff2e6',
   },
+
+  Hello: {
+    marginTop: 70,
+    marginLeft: 20,
+  },
+
+  helloText: {
+
+    fontWeight: 'bold',
+
+    fontSize: 32,
+
+  },
+
+  smallerText: {
+    fontSize: 12,
+
+  },
+
+  avatar: {
+
+    marginTop: 72,
+    marginLeft: 290,
+    position: 'absolute',
+
+  },
+
+
+  search: {
+    margin: 18,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flexDirection: "row",
+    width: "70%",
+  },
+
+  searchBar:{
+    padding: 10,
+    flexDirection: "row",
+    width: "95%",
+    backgroundColor: "#ffffff",
+    borderRadius: 15,
+    alignItems: "center",
+  },
+
+
+  input: {
+    fontSize: 20,
+    marginLeft: 10,
+    width: "90%",
+  },
+
 });
